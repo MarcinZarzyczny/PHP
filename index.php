abc0f51df9a2023d6450220b49a139a087e41c90
Marcin Zarzyczny <30557460+MarcinZarzyczny@users.noreply.github.com>
2025-01-29 19:53:59 +0100

    Add new files

--- index.php
+++ index.php
@@ -1,42 +1,24 @@
 <?php
-   $url = "http://www.mbnet.com.pl/dl.txt";
-
-   $filename = "losowanie.txt";
-   $year = "1977";
-   $month = "09";
-
-   $table = [];
-
-   if(!is_file($filename)){
-      $data = file_get_contents($url);
-      file_put_contents($filename, $data);
-   }
-
-   if(is_file($filename)){
-
-      $file = fopen($filename, "r");
-
-      while(!feof($file)){
-         $data = fgets($file);
-
-         if($data){
-         $pos = mb_strpos($data, $month.".".$year);
-         if($pos !== false){
-           // echo $data;
-            $t1 = explode(" ", $data);
-            $t2 = explode(".", $t1[1]);
-            $numbers = explode(",", $t1[2]);
-            $table[$t2[2]] [$t2[1]][$t2[0]] = $numbers;
-            echo("<br>");
-
-
-         } 
+function licznik(?string $filename = "Licznik.txt") : int
+{
+   if(!is_file($filename))
+   {
+      file_put_contents($filename, "1");
+      return (int)1;
+   }else{
+      $file = fopen($filename, "r+");
+      if(flock($file, LOCK_EX)){
+         $counter = (int)fread($file, filesize($filename));
+         $counter++;
+         fseek($file, 0);
+         fwrite($file, $counter);
+         flock($file, LOCK_UN);
       }
+      fclose($file);
+
+      return (int)$counter;
    }
-   fclose($file);
 }
-echo "<pre>";
-print_r($table);
-echo "</pre>";
+print "Jesteś " .Licznik(filename: "licznik.txt")." gościem na stronie.";
 
 ?>
